40dc3cec7643d4f2309795be73fc9751d7a494c1
pirsquared <sean.snv@gmail.com>
2017-12-10 23:28:23 -0800

    Another Day

---
+++ 2017/Day11_2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+dd = dict(n=0, ne=1, se=2, s=3, sw=4, nw=5)
+moves = list(map(dd.get, open('Day11.txt').read().split(',')))
+
+vectors = np.array([
+    [ 1,  0, -1],
+    [ 1, -1,  0],
+    [ 0, -1,  1],
+    [-1,  0,  1],
+    [-1,  1,  0],
+    [ 0,  1, -1]
+])
+
+(lambda a: (a[-1], a.max()))(np.abs(vectors[moves].cumsum(0)).max(1))
