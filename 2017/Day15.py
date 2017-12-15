1fd27f17bbaa4a833c7d63d8014a65b1f70729a6
pirsquared <sean.snv@gmail.com>
2017-12-14 21:30:26 -0800

    AoC Day 15

---
+++ 2017/Day15.py
@@ -0,0 +1,42 @@
+from itertools import islice
+
+inputA = 277
+inputB = 349
+testA = 65
+testB = 8921
+
+
+def gA(value, k=None):
+    while True:
+        if (k is None) or (value % k == 0):
+            yield value
+        value = (16807 * value) % 2147483647
+
+
+def gB(value, k=None):
+    while True:
+        if (k is None) or (value % k == 0):
+            yield value
+        value = (48271 * value) % 2147483647
+
+
+def judge(t):
+    a, b = t
+    c = 2 ** 16 - 1
+    return (c & a) == (c & b)
+
+
+def test(A, B, n, kA=None, kB=None):
+    return sum(map(judge, islice(zip(gA(A, kA), gB(B, kB)), n)))
+
+
+assert test(A=testA, B=testB, n=40000000) == 588
+assert test(A=testA, B=testB, n=5000000, kA=4, kB=8) == 309
+
+print(
+    test(A=inputA, B=inputB, n=40000000),
+    test(A=inputA, B=inputB, n=5000000, kA=4, kB=8)
+)
+
+
+# 592 320
